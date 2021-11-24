6f9c7fee7577683f494ceb3951e050569fcfbe76
Manuel Balanta <mbalantaq@gmail.com>
2021-11-24 05:34:59 -0500

    App done

--- FactoresdeRentabiliad/urls.py
+++ FactoresdeRentabiliad/urls.py
@@ -20,5 +20,7 @@ from . import views
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    path('', views.Swiset)
+    path('', views.Swiset),
+    path('Result/', views.Result, name = "Result"),
+
 ]
--- FactoresdeRentabiliad/views.py
+++ FactoresdeRentabiliad/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render
+import joblib 
+import numpy as np
+from sklearn.preprocessing import PolynomialFeatures
+
+
 #from django.http import HttpResponse
 
 def Swiset(request):
     #return HttpResponse("Hello world")
     return render(request, "Swiset.html")
 
-# Create your views here.
+def Result(request):
+    
+    cls = joblib.load("model.sav")
+
+    list = []
+    list.append(request.GET['entry'])
+    list.append(request.GET['close'])
+    list.append(request.GET['sl'])
+    list.append(request.GET['vpip'])
+    list.append(request.GET['porcentaje'])
+    list.append(request.GET['monetario'])
+    list.append(request.GET['commisions'])
+    list.append(request.GET['asset'])
+    list.append(request.GET['broker'])
+    list.append(0)
+   
+    list_f = [float(i) for i in list]
+
+    #poly = PolynomialFeatures(degree=2)
+    #list_f = poly.fit_transform(list_f)
+
+    vct = np.array(list_f)
+
+    #new_vct = vct.reshape(1,-1) 
+    
+    ans = cls.predict([vct])
+   
+    return render(request, "Result.html", {'ans':ans})
